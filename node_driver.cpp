b89e7069247492f2342c2ee2ed378318c96fe1a5
James Combs <jamescombs@Jamess-MacBook-Pro.local>
2018-09-13 00:27:50 -0500

    Setup skeleton code for node process

--- node_driver.cpp
+++ node_driver.cpp
@@ -2,8 +2,32 @@
 
 using namespace std;
 
-int main(int arc, char **argc)
+void sender(void *);
+void receiver(void *);
+
+int main(int argc, char **argv)
 {
-   cout << "Hello from node process." << endl;
+   if(argc != 4)
+   {
+      cerr << "[-] Error : Invalid number of arguments for node process." << endl;
+      return -1;
+   }
+
+   const string NID = argv[1];
+   const string HOSTNAME = argv[2];
+   const string PORT = argv[3];
+
+   cout << "Hello from node (" << NID << " " << HOSTNAME << " " << PORT << ")" << endl;
+
    return 0;
+}
+
+void sender(void *args)
+{
+
+}
+
+void receiver(void *args)
+{
+
 }
