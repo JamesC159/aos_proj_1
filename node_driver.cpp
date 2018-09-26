442b61278a1b2ee0c25b0e5e13f9037c91f3d33c
eteral00 <khoa.ho@utdallas.edu>
2018-09-25 21:15:55 -0500

    revised, compiled, test run
    Need to confirm the output.
    Then once the output is good, need to clean the code (currently quite messy).

--- node_driver.cpp
+++ node_driver.cpp
@@ -15,8 +15,9 @@
 #include <netinet/in.h>
 #include <netdb.h>
 
-/* POSIX semaphore library*/
+/* POSIX semaphore and pthread library*/
 #include <semaphore.h>
+#include <pthread.h>
 
 #include "Node.h"
 #include "nodeMsg.h"
@@ -112,9 +113,10 @@ hopCountMap[(this_node->nid)] = 0; // set my hop count from me = 0...
 terminable = false; // initiate var terminable
 int numOfDiscoveredNodes = 1; // discovered myself
 int numOfCompletedNodes = 0; // no one is done yet
-   // start sender and receiver threads with this node
-   threads.push_back(thread(sender, this_node));
+   
+   // start sender and receiver threads with this node, should start receiver before sender - as receiver acting as "server"
    threads.push_back(thread(receiver, this_node));
+   threads.push_back(thread(sender, this_node));
 
 // main thread will now be acting as master processor for receiver and sender threads
 
@@ -128,95 +130,100 @@ sem_wait(&outgoingQueue); // call the wait on semaphore for outgoing queue
 outgoingMsgQueue.push(msgInProcess); // push the msg to the outgoing queue
 sem_post(&outgoingQueue); // signal semaphore for outgoing queue
 
+bool hasIncomingQueue;
+sem_wait(&incomingQueue);
+hasIncomingQueue = !incomingMsgQueue.empty();
+sem_post(&incomingQueue);
 
 /* incoming -> processing -> outgoing */
-while ( (numOfCompletedNodes < num_nodes) || (!incomingMsgQueue.empty()) )
+while ( (numOfCompletedNodes < num_nodes) || (hasIncomingQueue) )
 {
     // if numOfCompletedNodes = num_nodes, but incoming msg queue is not empty, still need to finish its work backlog
-
-    sem_wait(&incomingQueue); // wait on the semaphore 
-    msgInProcess = incomingMsgQueue.front(); // get the first msg - one in front of the queue.
-    incomingMsgQueue.pop(); // pop the front of the queue.
-    sem_post(&incomingQueue); // signal the semaphore
-	switch (msgInProcess.msgCode)
-	{
-		case 0:
-		// discovery msg
-			if (msgInProcess.msgHop < hopCountMap[msgInProcess.oriSender])
-			{
-				hopCountMap[msgInProcess.oriSender] = msgInProcess.msgHop;
-				msgInProcess.msgHop++; // increment the hop
-				sem_wait(&outgoingQueue); // call the wait on semaphore for outgoing queue
-				outgoingMsgQueue.push(msgInProcess); // push the msg to the outgoing queue
-				sem_post(&outgoingQueue); // signal semaphore for outgoing queue 
-
-			}
-			/* the tweak
-			* if the msgHop >= hopCountMap[msgInProcess.oriSender]
-			* it means the node that is the oriSender of that msg has already been discovered previously by me,
-			* which also means I should have forwarded a similar msg from that oriSender before, which got to me through another middle node,
-			* thus, there is no need to forward another
-			* this would reduce the total number of msg in the network
-			*/
-
-		break;
-		case 1:
-			// completion/termination signal msg
-			if (completionMap[msgInProcess.oriSender] == false)
-			{
-				completionMap[msgInProcess.oriSender] = true; // set the status of that nodes to completed
-				numOfCompletedNodes++; // increment number of completed nodes
-				sem_wait(&outgoingQueue); // call the wait on semaphore for outgoing queue
-				outgoingMsgQueue.push(msgInProcess); // push the msg to the outgoing queue
-				sem_post(&outgoingQueue); // signal semaphore for outgoing queue 
-			}
-		break;
-		default:
-		break;
-	}
-    
-
-    // checking the number of discovered nodes
-    if ( numOfDiscoveredNodes < num_nodes )
+    if(hasIncomingQueue)
+    // only do the processing if incoming queue has something
     {
-        // reset numOfDiscoveredNodes to 0 then recount;
-        numOfDiscoveredNodes = 0;
-        for (int i = 0; i< num_nodes; i++)
+        sem_wait(&incomingQueue); // wait on the semaphore 
+        msgInProcess = incomingMsgQueue.front(); // get the first msg - one in front of the queue.
+        incomingMsgQueue.pop(); // pop the front of the queue.
+        sem_post(&incomingQueue); // signal the semaphore
+        switch (msgInProcess.msgCode)
         {
-            if ( (hopCountMap[i] < num_nodes) && (hopCountMap[i] >= 0) )
+            case 0:
+            // discovery msg
+                if (msgInProcess.msgHop < hopCountMap[msgInProcess.oriSender])
+                {
+                    hopCountMap[msgInProcess.oriSender] = msgInProcess.msgHop;
+                    msgInProcess.msgHop++; // increment the hop
+                    sem_wait(&outgoingQueue); // call the wait on semaphore for outgoing queue
+                    outgoingMsgQueue.push(msgInProcess); // push the msg to the outgoing queue
+                    sem_post(&outgoingQueue); // signal semaphore for outgoing queue 
+
+                }
+                /* the tweak
+                * if the msgHop >= hopCountMap[msgInProcess.oriSender]
+                * it means the node that is the oriSender of that msg has already been discovered previously by me,
+                * which also means I should have forwarded a similar msg from that oriSender before, which got to me through another middle node,
+                * thus, there is no need to forward another
+                * this would reduce the total number of msg in the network
+                */
+
+            break;
+            case 1:
+                // completion/termination signal msg
+                if (completionMap[msgInProcess.oriSender] == false)
+                {
+                    completionMap[msgInProcess.oriSender] = true; // set the status of that nodes to completed
+                    numOfCompletedNodes++; // increment number of completed nodes
+                    sem_wait(&outgoingQueue); // call the wait on semaphore for outgoing queue
+                    outgoingMsgQueue.push(msgInProcess); // push the msg to the outgoing queue
+                    sem_post(&outgoingQueue); // signal semaphore for outgoing queue 
+                }
+            break;
+            default:
+            break;
+        }
+        
+
+        // checking the number of discovered nodes
+        if ( numOfDiscoveredNodes < num_nodes )
+        {
+            // reset numOfDiscoveredNodes to 0 then recount;
+            numOfDiscoveredNodes = 0;
+            for (int i = 0; i< num_nodes; i++)
             {
-                numOfDiscoveredNodes++;
+                if ( (hopCountMap[i] < num_nodes) && (hopCountMap[i] >= 0) )
+                {
+                    numOfDiscoveredNodes++;
+                }
+            }
+
+            // if all nodes have just been discovered, push the completion signal msg to outgoing queue
+            if (numOfDiscoveredNodes == num_nodes)
+            {
+                // assemble a completion signal msg
+                msgInProcess.msgCode = 1;
+                msgInProcess.oriSender = (this_node->nid);
+                msgInProcess.msgHop = num_nodes;
+                msgInProcess.curSender = (this_node->nid);
+                sem_wait(&outgoingQueue); // call the wait on semaphore for outgoing queue
+                outgoingMsgQueue.push(msgInProcess); // push the msg to the outgoing queue
+                sem_post(&outgoingQueue); // signal semaphore for outgoing queue 
+
+                // change my completion status on my completion map to true
+                completionMap[(this_node->nid)] = true;
+                numOfCompletedNodes++; // increase numOfCompletedNodes by 1, me.
             }
         }
-
-        // if all nodes have just been discovered, push the completion signal msg to outgoing queue
-        if (numOfDiscoveredNodes == num_nodes)
-        {
-            // assemble a completion signal msg
-            msgInProcess.msgCode = 1;
-            msgInProcess.oriSender = (this_node->nid);
-            msgInProcess.msgHop = num_nodes;
-            msgInProcess.curSender = (this_node->nid);
-            sem_wait(&outgoingQueue); // call the wait on semaphore for outgoing queue
-            outgoingMsgQueue.push(msgInProcess); // push the msg to the outgoing queue
-            sem_post(&outgoingQueue); // signal semaphore for outgoing queue 
-
-            // change my completion status on my completion map to true
-            completionMap[(this_node->nid)] = true;
-            numOfCompletedNodes++; // increase numOfCompletedNodes by 1, me.
-        }
     }
+    sem_wait(&incomingQueue);
+    hasIncomingQueue = !incomingMsgQueue.empty(); // recheck if queue is empty yet
+    sem_post(&incomingQueue);
 }
 
 // set var terminable = true to signal sender and receiver threads that they can terminate
 terminable = true;
 
 
-   // join all threads before exiting main thread
-   for(int i = 0; i < threads.size(); i++)
-   {
-      threads[i].join();
-   }
 
     // find max hop count after discovery
    int maxHop = 1;
@@ -247,6 +254,12 @@ terminable = true;
    }
    cout << "End .!." << endl;
 
+
+   // join all threads before exiting main thread
+   for(int i = 0; i < threads.size(); i++)
+   {
+      threads[i].join();
+   }
 
 
    return 0;
@@ -300,45 +313,67 @@ void sender(Node *this_node)
         client_addr[i].sin_addr.s_addr = ((struct in_addr *)(receiverHost[i]->h_addr))->s_addr; 
         client_addr[i].sin_port = htons(neighborNode->port);  // get the port of direct neighbor #i
 
-        // connect to neighbor #i through given port
-        if ( connect( client_sd[i], (struct sockaddr *) &client_addr[i], sizeof(client_addr[i]) ) == -1 )
+        // connect to neighbor #i through given port, retry until success
+        while (connect( client_sd[i], (struct sockaddr *) &client_addr[i], sizeof(client_addr[i]) ) == -1)
+        {
+        
+        // initial design, only 1 connect attempt >> failed >> bad!  --Khoa
+        /*if ( connect( client_sd[i], (struct sockaddr *) &client_addr[i], sizeof(client_addr[i]) ) == -1 )
         {
             cerr << "[-] Error : Failed connecting to receiver socket of node " << neighborNode->nid << " from node " << this_node->nid << endl;
             return;
+        }*/
+
         }
+        cout << "Successfully connected to receiver socket of node " << neighborNode->nid << endl;
 
     }
 
     int prevSender; // to recognize previous sender so as not to rebound msg
     nodeMsg outgoingMsg;
-    while ( (!terminable) || (!outgoingMsgQueue.empty()) )
+
+    bool hasOutgoingQueue;
+    sem_wait(&outgoingQueue);
+    hasOutgoingQueue = !(outgoingMsgQueue.empty());
+    sem_post(&outgoingQueue);
+
+    while ( (!terminable) || (hasOutgoingQueue) )
     // while terminable is not set, or outgoing queue is not empty, do sending out msg from the queue
     {
         //
-        sem_wait(&outgoingQueue);
-        outgoingMsg = outgoingMsgQueue.front(); 
-        outgoingMsgQueue.pop();
-        sem_post(&outgoingQueue);
-
-        prevSender = outgoingMsg.curSender;
-        outgoingMsg.curSender = this_node->nid;
-        for (int i= 0; i< countOf1Hop; i++)
+        if(hasOutgoingQueue)
         {
-            neighborNode = (this_node->adj_lst[i]);
-            if ( (neighborNode->nid) != prevSender )
-            // if 1 sent to 2, then 2 is not rebounding it back to 1
+            sem_wait(&outgoingQueue);
+            outgoingMsg = outgoingMsgQueue.front(); 
+            outgoingMsgQueue.pop();            
+            sem_post(&outgoingQueue);
+
+            prevSender = outgoingMsg.curSender;
+            outgoingMsg.curSender = this_node->nid;
+            for (int i= 0; i< countOf1Hop; i++)
             {
-                if ( write(client_sd[i], &outgoingMsg, sizeof(outgoingMsg) ) == -1)
+                neighborNode = (this_node->adj_lst[i]);
+                if ( (neighborNode->nid) != prevSender )
+                // if 1 sent to 2, then 2 is not rebounding it back to 1
                 {
-                    cerr << "Error on write call" << endl;
-                    return;
+                    if ( write(client_sd[i], &outgoingMsg, sizeof(outgoingMsg) ) == -1)
+                    {
+                        cerr << "Error on write call" << endl;
+                        return;
+                    }
                 }
             }
         }
+        sem_wait(&outgoingQueue);
+        hasOutgoingQueue = !(outgoingMsgQueue.empty()); // recheck if queue is empty
+        sem_post(&outgoingQueue);
     }
    
-
-
+    for (int i = 0; i < countOf1Hop; i++)
+    {
+        close(client_sd[i]);
+    }
+    
    
 }
 
@@ -384,28 +419,30 @@ void receiver(Node *this_node)
 
     nodeMsg incomingMsg; // place holder for incoming msg
 
-   while(!terminable)
-   // while terminable status has not been set
-   {
-      // accept the client connection
-      if((client_sd = accept(server_sd, (struct sockaddr *)&server_addr, (socklen_t*)&addr_len)) < 0)
-      {
-         cerr << "[-] Error : Failed accepting client connection in receiver in node " << this_node->nid << endl;
-         return;
-      }
+    while(!terminable)
+    // while terminable status has not been set
+    {
+        // accept the client connection
+        if((client_sd = accept(server_sd, (struct sockaddr *)&server_addr, (socklen_t*)&addr_len)) < 0)
+        {
+            cerr << "[-] Error : Failed accepting client connection in receiver in node " << this_node->nid << endl;
+            return;
+        }
 
-      while( read(client_sd, &incomingMsg, sizeof(incomingMsg))  > 0 )
-      {
-          sem_wait(&incomingQueue); // wait on the semaphore for incoming queue
-          incomingMsgQueue.push(incomingMsg); // push the msg onto incoming queue
-          sem_post(&incomingQueue); // signal the semaphore
-      }
-
+        while( read(client_sd, &incomingMsg, sizeof(incomingMsg))  > 0 )
+        {
+            sem_wait(&incomingQueue); // wait on the semaphore for incoming queue
+            incomingMsgQueue.push(incomingMsg); // push the msg onto incoming queue
+            sem_post(&incomingQueue); // signal the semaphore
+        }
 
         /* I took this out, as we are using main thread as processor --Khoa */
       // start a new receiver processor thread for this client
       //threads.push_back(thread(receiverProcessor, server_sd, client_sd));
-   }
+    }
+    
+    close(client_sd);
+    close(server_sd);
 }
 
 /**
@@ -429,7 +466,7 @@ void receiver(Node *this_node)
  * @param sd The server socket descriptor.
  * @param cd The client socket descriptor..
  */
-void receiverProcessor(int sd, int cd)
+/*void receiverProcessor(int sd, int cd)
 {
    bool not_done = true;
 
@@ -450,4 +487,4 @@ void receiverProcessor(int sd, int cd)
 
    // close the client's connection
    close(cd);
-}
+}*/
